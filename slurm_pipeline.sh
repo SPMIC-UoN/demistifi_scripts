b7d5e2e2fe952a85baead8d2afc5027f115807da
Martin Craig <martin.craig@nottingham.ac.uk>
2023-02-28 13:23:12 +0000

    Need a version of python with nibabel, so activate the renal conda environ temporarily

--- slurm_pipeline.sh
+++ slurm_pipeline.sh
@@ -4,15 +4,18 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=16g
 #SBATCH --time=02:00:00
-#SBATCH --array=0-0
+#SBATCH --array=2-596
 #SBATCH --job-name=ukbbseg
 
 module load ukbbseg-img
 module load quantiphyse-img
 module load renal-preproc-img
 module load dcm2niix-img
+module load conda-img
+source activate renal
 
 DATA_DIR=/share/ukbiobank/Release_1_body/
 OUTDIR=/share/ukbiobank/body_seg2
+#SLURM_ARRAY_TASK_ID=0
 
-python3 -u demistifi_pipeline.py --input "${DATA_DIR}" --output "${OUTDIR}" --subjids subjids.test --subjid-idx=${SLURM_ARRAY_TASK_ID}
+python3 -u demistifi_pipeline.py --input "${DATA_DIR}" --output "${OUTDIR}" --subjids subjids --subjid-idx=${SLURM_ARRAY_TASK_ID}
